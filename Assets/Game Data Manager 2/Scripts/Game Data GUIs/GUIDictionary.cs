101e14288cadadda429e88d51730604fa4007891
Gary Kearns <gckearns@gmail.com>
2017-01-12 16:01:21 -0500

    Update and rename GUIMember.cs to GUIDictionary.cs
    Copied class body from GUIString and renamed types.
    //TODO adapt members to appropriately use StringIntDictionary class.

---
+++ Assets/Game Data Manager 2/Scripts/Game Data GUIs/GUIDictionary.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameDataManager
+{
+    public class GUIDictionary
+    {
+        public string label { get; private set;}
+        public string value { get; set; }
+        public GUIDictionary(string label)
+        {
+            this.label = label;
+            value = "";
+        }
+        
+        public GUIDictionary(string label, string value)
+        {
+            this.label = label;
+            this.value = value;
+        }
+        
+        public override string ToString()
+        {
+            return string.Format("[GUIDictionary]{0}:{1}",label,value);
+        }
+    }
+}
