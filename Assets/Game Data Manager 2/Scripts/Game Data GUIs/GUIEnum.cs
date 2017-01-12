46e1a8f82f3286cca622289d8759fc22ee308cbc
Gary Kearns <gckearns@gmail.com>
2017-01-12 10:27:30 -0500

    Update GUIEnum.cs
    Matched updated GUIString style/formatting

--- Assets/Game Data Manager 2/Scripts/Game Data GUIs/GUIEnum.cs
+++ Assets/Game Data Manager 2/Scripts/Game Data GUIs/GUIEnum.cs
@@ -5,20 +5,21 @@ namespace GameDataManager
 {
     public struct GUIEnum
     {
-        public string label;
-        public System.Enum value;
-
+        public string label { get; private set; }
+        public System.Enum value { get; set; }
         public GUIEnum(string label)
         {
             this.label = label;
-
             value = null;
         }
-
         public GUIEnum(string label, System.Enum value)
         {
             this.label = label;
             this.value = value;
+        }
+        public override string ToString()
+        {
+            return string.Format("[GUIEnum]{0}:{1}",label,value);
         }
     }
 }
