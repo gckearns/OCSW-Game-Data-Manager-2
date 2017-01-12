a930703338f15f29ffae1c65990c79e31c1f2f98
Gary Kearns <gckearns@gmail.com>
2017-01-12 10:24:23 -0500

    Update GUIInt.cs
    Matched updated GUIString class style/formatting

--- Assets/Game Data Manager 2/Scripts/Game Data GUIs/GUIInt.cs
+++ Assets/Game Data Manager 2/Scripts/Game Data GUIs/GUIInt.cs
@@ -4,20 +4,23 @@ using System.Collections;
 namespace GameDataManager
 {
     public struct GUIInt
-    {
-        public string label;
-        public int value;
-
+    {        
+        public string label { get; private set; }        
+        public int value { get; set; }
+        
         public GUIInt(string label)
         {
             this.label = label;
             value = 0;
         }
-
         public GUIInt(string label, int value)
         {
             this.label = label;
             this.value = value;
+        }
+        public override string ToString()
+        {
+            return string.Format("[GUIInt]{0}:{1}",label,value);
         }
     }
 }
